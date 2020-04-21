d3d7ce452a0ba6c3d08d47d527e25f7f8a0a7e3a
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-04-21 12:50:16 +0600

    Create 959A - Mahmoud and Ehab and the even-odd game.java

---
+++ 959A - Mahmoud and Ehab and the even-odd game.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution{
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String args[]){
+        int n = scan.nextInt();
+        
+        if(n%2 == 0){
+            System.out.println("Mahmoud");
+        }
+        else{
+            System.out.println("Ehab");
+        }
+    }
+    
+}
