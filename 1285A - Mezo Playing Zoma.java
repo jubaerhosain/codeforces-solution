9dac46eb5f27ce86dc5c4b1d31295baa30967570
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-04-21 19:09:17 +0600

    Create 1285A - Mezo Playing Zoma.java

---
+++ 1285A - Mezo Playing Zoma.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution{
+    static Scanner scan = new Scanner(System.in);
+    
+    public static void main(String args[]){
+        int n = scan.nextInt();
+        String s = scan.next();
+        
+        System.out.println(n+1);
+    }
+    
+}
