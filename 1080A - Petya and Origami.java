cf622d8c75e9006a552b80a775208011c96ba921
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-04-23 20:17:53 +0600

    Create 1080A - Petya and Origami.java

---
+++ 1080A - Petya and Origami.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution{
+    static Scanner scan = new Scanner(System.in);
+    
+    public static void main(String args[]){
+        long n = scan.nextLong();
+        long k = scan.nextLong();
+        
+        long red = (long)Math.ceil((n*2) / k);
+        long green = (long)Math.ceil((n*5) / k);
+        long blue = (long)Math.ceil((n*8) / k);
+        
+        System.
+        
+        System.out.println(red);
+        System.out.println(green); 
+        System.out.println(blue);
+    }
+    
+}
