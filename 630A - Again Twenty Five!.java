d10cc73f2f8db1e6f4e743fe6ef19c7d5865a67d
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-04-26 15:01:27 +0600

    Create 630A - Again Twenty Five!.java

---
+++ 630A - Again Twenty Five!.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution{
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String args[]){
+        String n = scan.next();
+        
+        System.out.println(25);
+    }
+    
+}
