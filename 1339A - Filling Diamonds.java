0e45552236ffaa9d27e39dda8e2c4c35a420d013
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-01-18 00:05:27 +0600

    Create 1339A - Filling Diamonds.java

---
+++ 1339A - Filling Diamonds.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        int test = scan.nextInt();
+        
+        while(test-- > 0) {
+            System.out.println(scan.nextInt());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
