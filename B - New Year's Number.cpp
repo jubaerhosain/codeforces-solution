9670eacd5a47c535baddaf9c32dd994d2b0acbed
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-01-30 10:51:16 +0600

    Create B - New Year's Number.cpp

---
+++ B - New Year's Number.cpp
@@ -0,0 +1,42 @@
+/*************Headers*************/
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<map>
+using namespace std;
+/*************preprocessors*************/
+#define add push_back
+#define sortVector(v) sort(v.begin(), v.end()); 
+#define sortString(s) sort(s.begin(), s.end()); 
+#define sortArray(arr, n) sort(arr, arr+n)
+/*************Data type preprocessors*************/
+#define long long long
+/*************Global variable*************/
+int inf = 1e9;
+/*************User defined function*************/
+void solve() {
+    int test;
+    cin >> test;
+
+    while(test-- > 0) {
+        int n;
+        cin >> n;
+
+        if(n < 2020) {
+            cout << "NO" << endl;
+        } else {
+            int rem = n%2020;
+            int div = n/2020;
+            if(rem <= div) cout << "YES" << endl;
+            else cout << "NO" << endl;
+        }
+    }
+}
+
+/*************main function*************/
+int main() {
+    solve();
+    return 0;
+}
