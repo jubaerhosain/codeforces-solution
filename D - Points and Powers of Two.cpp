9b050fb7b6aa183926d8dfebd6e09a0609e51ba1
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-09 22:30:55 +0600

    Create D - Points and Powers of Two.cpp

---
+++ D - Points and Powers of Two.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define rall(v)                 v.rbegin(), v.rend()
+#define des()                   greater<>()
+#define rep(i, a, n)            for(int i = a; i < n; i++)  
+#define rrep(i, n, a)           for(int i = n; i >= a; i--)
+#define bitcount(x)             __builtin_popcount(x)
+#define bitcountll(x)           __builtin_popcountll(x)
+#define umap                    unordered_map
+#define uset                    unordered_set
+#define add                     push_back
+#define endl                    '\n'
+#define println                 cout << endl;
+#define scan_array(arr, n)      for(int i = 0; i < n; i++) cin >> arr[i];
+#define print_array(arr)        for(auto val: arr) cout << val << " ";
+#define print(var)              cout << var << endl
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+const int eps = 1e-9;
+const int inf = INT_MAX;
+
+/*************User defined function*************/
+void solve() {
+    int n; cin >> n;
+
+    vector<ll> v(n);
+    scan_array(v, n);
+    sort(all(v));
+
+    vector<ll> ans;
+    rep(i, 0, n) {
+        rep(k, 0, 32) {
+            ll l = v[i]-(1<<k);
+            ll r = v[i]+(1<<k);
+            vector<ll> tmp = {v[i]};
+            if(binary_search(all(v), l)) tmp.add(l);
+            if(binary_search(all(v), r)) tmp.add(r);
+            if(tmp.size() > ans.size()) ans = tmp;
+        }
+    }
+
+    print(ans.size());
+    print_array(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    // int test;
+    // cin >> test;
+    // while(test-- > 0) {
+    //     solve();
+    // }
+
+    solve();
+
+    return 0;
+}    
