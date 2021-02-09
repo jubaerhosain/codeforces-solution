3fd31efeaeee8f982b7cf08e888698bc12e956ce
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-09 22:28:47 +0600

    Create A - Diverse Team.cpp

---
+++ A - Diverse Team.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define rall(v)                 v.rbegin(), v.rend()
+#define des()                   greater<>()
+#define rep(i, a, n)            for(int i = a; i < n; i++)  
+#define rrep(i, n, a)           for(int i = n; i >= a; i--)
+#define bitcount(x)             __builtin_popcount(x)
+#define bitcountll(x)           __builtin_popcountll(x)
+#define umap                    unordered_map
+#define uset                    unordered_set
+#define add                     push_back
+#define endl                    '\n'
+#define println                 cout << endl;
+#define scan_array(arr, n)      for(int i = 0; i < n; i++) cin >> arr[i];
+#define print_array(arr)        for(auto val: arr) cout << val << " ";
+#define print(var)              cout << var << endl
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+const int eps = 1e-9;
+const int inf = INT_MAX;
+
+/*************User defined function*************/
+void solve() {
+    int n, k; cin >> n >> k;
+
+    vector<int> indx(105, -1);
+    uset<int> st;
+    rep(i, 1, n+1) {
+        int x; cin >> x;
+        indx[x] = i;
+        st.insert(x);
+    }
+
+    if(st.size() < k) {
+        print("NO");
+    } else {
+        print("YES");
+        int pos = 1;
+        rep(i, 1, indx.size()) {
+            if(indx[i] > -1 && pos <= k) {
+                cout << indx[i] << " ";
+                pos++;
+            }
+        }
+        
+    }
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    // int test;
+    // cin >> test;
+    // while(test-- > 0) {
+    //     solve();
+    // }
+
+    solve();
+
+    return 0;
+}    
