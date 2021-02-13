9f948a1952bb64fa49bbb59c7501bb457e2365e3
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-13 19:59:02 +0600

    Create A - Mishka and Contest.cpp

---
+++ Codeforces Round #490 (Div. 3)/A - Mishka and Contest.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define rall(v)                 v.rbegin(), v.rend()
+#define des()                   greater<int>()
+#define rep(i, a, n)            for(int i = a; i < n; i++)  
+#define rrep(i, n, a)           for(int i = n; i >= a; i--)
+#define bitcount(x)             __builtin_popcount(x)
+#define bitcountll(x)           __builtin_popcountll(x)
+#define pq                      priority_queue
+#define umap                    unordered_map
+#define uset                    unordered_set
+#define add                     push_back
+#define pob                     pop_back  
+#define endl                    '\n'    //in interactive problem should not use this
+#define println                 cout << endl
+#define scan_array(arr, n)      for(int i = 0; i < n; i++) { cin >> arr[i]; }
+#define print_array(arr, n)     for(int i = 0; i < n; i++) { cout << arr[i] << " "; } println;
+#define print(var)              cout << var << endl
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+const int eps = 1e-9;
+const int inf = INT_MAX;
+const int N = 1e5+10;
+
+/*************User defined function*************/
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+    scan_array(a, n);
+
+    int ans = 0;
+    int l = 0, r = n-1;
+    while(l <= r) {
+        if(a[l] <= k) {
+            ans++, l++;
+        } else if(a[r] <= k) {
+            ans++, r--;
+        } else {
+            break;
+        }
+    }
+
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    // int test;
+    // cin >> test;
+    // while(test-- > 0) {
+    //     solve();
+    // }
+
+    solve();
+
+    return 0;
+}    
