4af098e31c8c824f8fb7d040cfa8363f95c8c91c
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-13 15:15:59 +0600

    Create A - Yet Another String Game.cpp

---
+++ Codeforces Round #700 (Div. 2)/A - Yet Another String Game.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define rall(v)                 v.rbegin(), v.rend()
+#define des()                   greater<>()
+#define rep(i, a, n)            for(int i = a; i < n; i++)  
+#define rrep(i, n, a)           for(int i = n; i >= a; i--)
+#define bitcount(x)             __builtin_popcount(x)
+#define bitcountll(x)           __builtin_popcountll(x)
+#define umap                    unordered_map
+#define uset                    unordered_set
+#define add                     push_back
+#define endl                    '\n'
+#define println                 cout << endl;
+#define scan_array(arr, n)      for(int i = 0; i < n; i++) cin >> arr[i];
+#define print_array(arr)        for(auto val: arr) cout << val << " ";
+#define print(var)              cout << var << endl
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+const int eps = 1e-9;
+const int inf = INT_MAX;
+
+/*************User defined function*************/
+void solve() {
+    string s;
+    cin >> s;
+    
+    bool turn = true;
+    rep(i, 0, s.size()) {
+        if(turn) {
+            if(s[i] == 'a') s[i] = 'b';
+            else s[i] = 'a';
+            turn = false;
+        } else {
+            if(s[i] == 'z') s[i] = 'y';
+            else s[i] = 'z';
+            turn = true;
+        }
+    }
+
+    print(s);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    int test;
+    cin >> test;
+    while(test-- > 0) {
+        solve();
+    }
+
+    return 0;
+}    
