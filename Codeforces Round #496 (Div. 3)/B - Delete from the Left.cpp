7f964b17248e0330ac332b712d3b995577fd77b6
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-14 22:55:18 +0600

    Create B - Delete from the Left.cpp

---
+++ Codeforces Round #496 (Div. 3)/B - Delete from the Left.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define rall(v)                 v.rbegin(), v.rend()
+#define des()                   greater<int>()
+#define rep(i, a, n)            for(int i = a; i < n; i++)  
+#define rrep(i, n, a)           for(int i = n; i >= a; i--)
+#define pqueue                  priority_queue
+#define umap                    unordered_map
+#define uset                    unordered_set
+#define pub                     push_back
+#define pob                     pop_back  
+#define endl                    '\n'    //in interactive problem should not use this
+#define println                 cout << endl
+#define scan_array(arr, n)      for(int i = 0; i < n; i++) { cin >> arr[i]; }
+#define print_array(arr, n)     for(int i = 0; i < n; i++) { cout << arr[i] << " "; } println;
+#define print(var)              cout << var << endl
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll; 
+const int eps = 1e-9;
+const int inf = INT_MAX;
+const int N = 2e5+10;
+
+/*************User defined function*************/
+void solve() {
+    string s, t;
+    cin >> s >> t;
+
+    int cnt = 0;
+    int l1 = s.size()-1;
+    int l2 = t.size()-1;
+    while(l1 >= 0 && l2 >= 0) {
+        if(s[l1] == t[l2]) {
+            cnt++;
+        } else {
+            break;
+        }
+        l1--, l2--;
+    }
+
+    int ans = (s.size()-cnt)+(t.size()-cnt);
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    // int test;
+    // cin >> test;
+    // while(test-- > 0) {
+    //     solve();
+    // }
+
+    solve();
+
+    return 0;
+}    
