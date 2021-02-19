e27cd0b65de387b55d701cc6c521c22615afc30f
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-19 11:01:13 +0600

    Create B - Eastern Exhibition.cpp

---
+++ Codeforces Round #703 (Div. 2)/B - Eastern Exhibition.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define rall(v)                 v.rbegin(), v.rend()
+#define des()                   greater<int>()
+#define rep(i, a, n)            for(int i = a; i < n; i++)  
+#define rrep(i, n, a)           for(int i = n; i >= a; i--)
+#define pqueue                  priority_queue
+#define umap                    unordered_map
+#define uset                    unordered_set
+#define pub                     push_back
+#define pob                     pop_back  
+#define endl                    '\n' //in interactive problem should not use this
+#define println                 cout << endl
+#define scan_array(arr, n)      for(int i = 0; i < n; i++) { cin >> arr[i]; }
+#define print_array(arr, n)     for(int i = 0; i < n; i++) { cout << arr[i] << " "; } println;
+#define print(var)              cout << var << endl
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+const int p = 1e9+7;
+const int eps = 1e-9;
+const int inf = INT_MAX;
+const int N = 1e5+5;
+
+/*************User defined function*************/
+void solve() {
+    int n; cin >> n;
+
+    vector<int> x(n), y(n);
+    rep(i, 0, n) {
+        cin >> x[i] >> y[i];
+    }
+
+    if(n&1) {
+        print(1);
+        return;
+    }
+
+    sort(all(x));
+    sort(all(y));
+    ll x1 = x[n/2]-x[n/2-1]+1;
+    ll y1 = y[n/2]-y[n/2-1]+1;
+
+    print(x1*y1);  
+}
+
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    int test;
+    cin >> test;
+    while(test-- > 0) {
+        solve();
+    }
+
+    //solve();
+
+    return 0;
+}    
