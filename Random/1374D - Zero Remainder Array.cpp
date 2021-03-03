a21645661c464bd7f61bd299e6625e45a89f3305
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-03 14:35:41 +0600

    Create 1374D - Zero Remainder Array.cpp

---
+++ Random/1374D - Zero Remainder Array.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(ll i = s; i < n; i++)
+#define rrep(i, n, s)           for(ll i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+const int inf = INT_MAX;
+const int N = 2e5+5;
+
+/*************User defined function*************/
+void solve() {
+    ll n, k; 
+    cin >> n >> k;
+
+    map<int, int> mp;
+    rep(i, 0, n) {
+        int x; cin >> x;
+        int rem = x % k;
+        if(rem > 0) mp[k-rem]++;
+    } 
+
+    ll ans = 0;
+    for(auto p: mp) {
+        ll curr = p.first+1;
+        curr += ((p.second-1)*k);
+        ans = max(ans, curr);
+    }
+
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
