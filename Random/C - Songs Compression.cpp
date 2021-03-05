7265af3bb2674933f1c681497da051587d1ad389
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-05 12:42:49 +0600

    Create C - Songs Compression.cpp

---
+++ Random/C - Songs Compression.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(ll i = s; i < n; i++)
+#define rrep(i, n, s)           for(ll i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int inf = INT_MAX;
+const int N = 2e5+5;
+
+/*************User defined function*************/
+int cmp(pll a, pll b) {
+    return a.first-a.second < b.first-b.second;
+}
+
+void solve() {
+    ll n, m;
+    cin >> n >> m;
+
+    vector<pll> v(n);
+    rep(i, 0, n) {
+        int a, b;
+        cin >> a >> b;
+        v[i] = {a, b};
+    }
+
+    sort(all(v), cmp);
+
+    // for(auto p: v) {
+    //     cout << p.first << " " << p.second << endl;
+    // }
+
+    rep(i, 1, n) {
+        v[i].first += v[i-1].first;
+    }
+
+    ll sum = 0, cnt = 0;
+    rrep(i, n, 1) {
+        if(sum+v[i-1].first <= m) {
+            break;
+        } else {
+            sum += v[i-1].second;
+            cnt++;
+        }
+    }
+
+    if(sum > m) {
+        print(-1);
+    } else {
+        print(cnt);
+    }
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
