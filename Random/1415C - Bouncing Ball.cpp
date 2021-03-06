8094e2b7a538ed5f228c7f04ad843d60e608945c
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-06 19:08:21 +0600

    Create 1415C - Bouncing Ball.cpp

---
+++ Random/1415C - Bouncing Ball.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(ll i = s; i < n; i++)
+#define rrep(i, n, s)           for(ll i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int inf = INT_MAX;
+const int N = 4e3+5;
+
+/*************User defined function*************/
+void solve() {
+    int n, p, k;
+    cin >> n >> p >> k;
+    p--;
+
+    string str;
+    cin >> str;
+
+    int x, y;
+    cin >> x >> y;
+
+    ll ans = inf;
+    vector<int> dp(n);
+    rrep(i, n-1, 0) {
+        dp[i] = (str[i] == '0');
+        if(i+k < n) dp[i] += dp[i+k];
+        if(i >= p) ans = min(ans, (i-p)*y + dp[i]*x);
+    }
+
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
