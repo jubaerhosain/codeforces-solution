baad06344adafb653b9cd51e3cd4ccc380855fff
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-09 01:16:35 +0600

    Create 499B - Lecture.cpp

---
+++ Random/499B - Lecture.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int inf = INT_MAX;
+const int N = 4e3+5;
+
+/*************User defined function*************/
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    map<string, string> mp;
+    while(m--) {
+        string a, b;
+        cin >> a >> b;
+        mp[a] = b;
+    }
+
+    vector<string> ans(n);
+    for(string &i: ans) cin >> i;
+
+    for(string &i: ans) {
+        if(i.size() > mp[i].size()) i = mp[i];
+    }
+    
+    for(string &i: ans) cout << i << " ";
+}
+    
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
