7b8b64268bbd0e3f6f7d1a683da03459f17c98d1
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-09 16:29:57 +0600

    Create 1194C - From S To T.cpp

---
+++ Random/1194C - From S To T.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int inf = INT_MAX;
+const int N = 4e3+5;
+
+/*************User defined function*************/
+void solve() {
+    string s, t, p;
+    cin >> s >> t >> p;
+
+    int i = 0, j = 0;
+    while(i < s.size() && j < t.size()) {
+        if(s[i] == t[j]) i++, j++;
+        else j++;
+    }
+
+    if(i < s.size()) {
+        print("NO");
+        return;
+    }
+
+    vector<int> c1(26, 0), c2(26, 0);
+    for(char c: s) c1[c-'a']++;
+    for(char c: p) c1[c-'a']++;
+    for(char c: t) c2[c-'a']++;
+
+    rep(i, 0, 26) {
+        if(c1[i] < c2[i]) {
+            print("NO");
+            return;
+        }
+    }
+
+    print("YES");
+}
+    
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
