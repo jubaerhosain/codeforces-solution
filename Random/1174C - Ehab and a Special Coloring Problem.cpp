4545bd428c3517982e9c31f65d4405681f515c7e
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-13 09:44:42 +0600

    Create 1174C - Ehab and a Special Coloring Problem.cpp

---
+++ Random/1174C - Ehab and a Special Coloring Problem.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queues
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+const int inf = INT_MAX;
+const int N = 1e5+5;
+
+/*************User defined function*************/
+void solve() {
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    vector<int> ans(n+1, 0);
+    rep(i, 2, n+1) {
+        if(ans[i] == 0) {
+            ans[i] = ++cnt;
+            for(int j = i; j <= n; j += i) {
+                ans[j] = ans[i];
+            }
+        }
+    }
+
+    rep(i, 2, n+1) cout << ans[i] << " ";
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
