d51b2d0b76b40cea8979a9c68bed711b2ebd4b29
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-13 09:03:23 +0600

    Create 1487B - Cat Cycle.cpp

---
+++ Random/1487B - Cat Cycle.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queues
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+const int inf = INT_MAX;
+const int N = 4e3+5;
+
+/*************User defined function*************/
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    k--;
+    if(n % 2 == 0) {
+        int ans = k % n;
+        print(ans+1);
+    } else {
+        int d = n / 2;
+        int gap = k / d;
+        int ans = (gap+k) % n;
+        print(ans+1);
+    }
+
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
