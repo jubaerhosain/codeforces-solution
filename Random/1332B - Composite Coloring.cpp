ebac73a250a282d7f8a30450c3ec53ef37865b51
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-15 21:28:20 +0600

    Create 1332B - Composite Coloring.cpp

---
+++ Random/1332B - Composite Coloring.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queues
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+const ll inf = 1e18;
+const int N = 1e5+5;
+
+/*************User defined function*************/
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n), ans(n, 0);
+    rep(i, 0, n) cin >> v[i];
+
+    int m = 1;
+    int p[] = {2,3,5,7,11,13,17,19,23,29,31};
+    rep(i, 0, 11) {
+        bool flag = false;
+        rep(j, 0, n) {
+            if(v[j]%p[i] == 0 && ans[j] == 0) {
+                ans[j] = m;
+                flag = true;
+            }
+        }
+        if(flag) m++;
+    }
+
+    print(m-1);
+    for(int i: ans) cout << i << " ";
+    cout << endl;
+} 
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
