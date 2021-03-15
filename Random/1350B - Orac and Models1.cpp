d88dc5707c2406ecb43623abda175c5f9a9cbf81
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-15 15:04:34 +0600

    Create 1350B - Orac and Models1.cpp

---
+++ Random/1350B - Orac and Models1.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queues
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+const ll inf = 1e18;
+const int N = 1e5+5;
+
+/*************User defined function*************/
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n+1, 0), dp(n+1, 1);
+    rep(i, 1, n+1) cin >> v[i];
+
+    int mx = 1;
+    rep(i, 1, n+1) {
+        for(int j = i+i; j <= n; j += i) {
+            if(v[i] < v[j]) {
+                dp[j] = max(dp[j], dp[i]+1);
+                mx = max(mx, dp[j]);
+            }
+        }
+    }
+
+    print(mx);
+} 
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
