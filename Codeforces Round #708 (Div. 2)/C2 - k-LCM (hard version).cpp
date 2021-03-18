f5939bc59b90a8da2d50fef48c0e65d6cd426664
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-18 10:49:20 +0600

    Create C2 - k-LCM (hard version).cpp

---
+++ Codeforces Round #708 (Div. 2)/C2 - k-LCM (hard version).cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queues
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+const ll inf = 1e18;
+const int N = 1e5+5;
+
+/*************User defined function*************/
+void out(int a, int b, int c) {
+    cout << a << " " << b << " " << c << endl;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k; 
+
+    for(int i = 1; i <= k-3; i++) cout << 1 << " ";
+
+    n -= (k-3);
+    k = 3;
+    if(n%k == 0) {
+        int a = n/k;
+        out(a, a, a);
+    } else if(n%2) {
+        out(1, n/2, n/2);
+    } else {
+        int h = n/2;
+        if(h%2) {
+            out(2, h-1, h-1);
+        } else {
+            out(h/2, h/2, h);
+        }
+    }
+
+} 
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
