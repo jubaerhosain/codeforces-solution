f8032851e5f7af2a380b3b533a95ca0c893ce3bc
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-21 14:56:47 +0600

    Create 1426C - Increase and Copy.cpp

---
+++ Random/1426C - Increase and Copy.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 1e5+5;
+
+/*************User defined function*************/
+void out(int a, int b) {
+    cout << a+1 << " " << b+1 << endl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int sq = sqrt(n);
+    
+    if(sq*sq == n) {
+        print(2*sq-2); 
+    } else if(sq*(sq+1) >= n) {
+        print(2*sq-1);
+    } else {
+        print(2*sq);
+    }
+} 
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
