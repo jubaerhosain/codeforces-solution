fcdd7ec03a9ff4bc40019a512d34ad9cde634947
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-22 22:21:23 +0600

    Create 1335D - Anti-Sudoku.cpp

---
+++ 1335D - Anti-Sudoku.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 1e5+5;
+
+/*************User defined function*************/
+void out(int a, int b) {
+    cout << a << " " << b << endl;
+}
+
+void solve() {
+    int n = 9;
+
+    string s;
+    rep(i, 0, n) {
+        cin >> s;
+        for(char &c: s) if(c == '1') c = '2';
+        print(s);
+    }  
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
