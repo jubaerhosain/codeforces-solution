e89108fcb7c33e0978bb2c6af07d76bf69699a4e
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-24 23:55:04 +0600

    Create 1004C - Sonya and Robots.cpp

---
+++ 1004C - Sonya and Robots.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 2e5+5;
+
+/*************User defined function*************/
+void out(int a, int b, int c) {
+    cout << a << " " << b << " " << c << endl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n), dp(n, 0);
+    rep(i, 0, n) cin >> a[i];
+
+    map<int, int> mp;
+    mp[a[n-1]] = 1;
+    rrep(i, n-2, 0) {
+        if(mp[a[i]]) dp[i] = dp[i+1];
+        else dp[i] = 1 + dp[i+1];
+        mp[a[i]]++;
+    }
+
+    ll ans = 0;
+    set<int> used;
+    rep(i, 0, n) {
+        if(used.count(a[i]) == 0) {
+            ans += dp[i];
+            used.insert(a[i]);
+        }
+    } 
+
+    for(auto p: mp) {
+        if(p.second > 1) ans++;
+    }
+
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
