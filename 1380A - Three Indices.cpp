9058bf47effbdc353c3c74b958e160a2f9228ba0
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-25 10:26:02 +0600

    Create 1380A - Three Indices.cpp

---
+++ 1380A - Three Indices.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 2e5+5;
+
+/*************User defined function*************/
+void out(int a, int b, int c) {
+    cout << a << " " << b << " " << c << endl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    rep(i, 0, n) cin >> a[i];
+
+    rep(i, 1, n-1) {
+        if(a[i] > a[i-1] && a[i] > a[i+1]) {
+            print("YES");
+            out(i, i+1, i+2);
+            return;
+        }
+    }
+
+    print("NO");
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
