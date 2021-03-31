907b6e745a412bb1c8c558cb8118e7ea05c6939b
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-03-31 11:56:53 +0600

    Create 1237B - Balanced Tunnel.cpp

---
+++ 1237B - Balanced Tunnel.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 1e5+5;
+
+/*************User defined function*************/
+void out(int a, int b, int c) {
+    cout << a << " " << b << " " << c << endl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    set<int> st;
+    vector<int> inx(n+1);
+    vector<int> a(n), b(n);
+    rep(i, 0, n) {
+        cin >> a[i];
+        inx[a[i]] = i;
+        st.insert(i);
+    }
+
+    rep(i, 0, n) cin >> b[i];
+
+    int i = 0;
+    int ans = 0;
+    while(i < n) {
+        auto ft = st.begin();
+        if(*ft == inx[b[i]]) {
+            st.erase(*ft);
+            i++;
+        } else {
+            ft = st.lower_bound(inx[b[i]]);
+            st.erase(*ft);
+            i++;
+            ans++;
+        }
+    }
+
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
