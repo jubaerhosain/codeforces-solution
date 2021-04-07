663cfea7416fa5b804691e11d1d17095a21d9fb7
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-04-07 22:27:36 +0600

    Create 602B - Approximating a Constant Range.cpp

---
+++ 602B - Approximating a Constant Range.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 2e5+5;
+
+/*************User defined function*************/
+void out(int a, int b) {
+    cout << a << " " << b << endl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    rep(i, 0, n) cin >> v[i];
+
+    int ans = 2;
+    int l = 0, r = 2;
+    map<int, int> mp;
+    mp[v[0]]++;
+    mp[v[1]]++;
+    while(r < n) {
+        auto p = mp.begin();
+        auto e = --mp.end();
+        if(abs(p->first-v[r]) <= 1 && abs(e->first-v[r]) <= 1) {
+            mp[v[r]]++;
+            r++;
+        } else {
+            mp[v[l]]--;
+            if(mp[v[l]] == 0) mp.erase(v[l]);
+            l++;
+        }
+        ans = max(ans, r-l);
+    }
+
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
