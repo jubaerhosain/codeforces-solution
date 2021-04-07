c53ad0930e41ea0538a15e375c48265b538f54b1
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-04-07 14:29:45 +0600

    Create 1190A - Tokitsukaze and Discard Items.cpp

---
+++ 1190A - Tokitsukaze and Discard Items.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 2e5+5;
+
+/*************User defined function*************/
+void out(int a, int b) {
+    cout << a << " " << b << endl;
+}
+
+int a[N];
+
+void solve() {
+    ll n, m, k;
+    cin >> n >> m >> k;
+
+    vector<ll> v(m);
+    rep(i, 0, m) cin >> v[i];
+
+    sort(all(v), greater<>());
+
+    ll r = k;
+    int ans = 0;
+    while(!v.empty()) {
+        int cnt = 0, flag = 0;
+        while(!v.empty() && v.back() <= r) {
+            cnt++;
+            flag = 1;
+            v.pop_back();
+        }
+
+        if(flag) {
+            r += cnt;
+            ans++;
+        }
+        else {
+            ll d = ceil((ld)(v.back()-r)/k);
+            r += (d*k);
+        }
+
+    }
+
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
