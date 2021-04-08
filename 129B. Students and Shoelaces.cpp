1080d59b0e666271d041e5d2a95a876c6f37c6c6
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-04-08 19:02:01 +0600

    Create 129B. Students and Shoelaces.cpp

---
+++ 129B. Students and Shoelaces.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 2e5+5;
+
+/*************User defined function*************/
+void out(int a, int b) {
+    cout << a << " " << b << endl;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    map<int, set<int>> mp;
+    rep(i, 0, m) {
+        int a, b;
+        cin >> a >> b;
+        mp[a].insert(b);
+        mp[b].insert(a);
+    }
+
+    int ans = 0;
+    while(true) {
+        vector<pii> v;
+        for(auto p: mp) {
+            if(p.second.size() == 1) {
+                v.push_back({p.first, *p.second.begin()});
+            }
+        }
+
+        if(v.size() == 0) break;
+        for(auto p: v) {
+            mp[p.first].erase(p.second);
+            mp[p.second].erase(p.first);
+        }
+        ans++;
+    }
+
+    print(ans);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
