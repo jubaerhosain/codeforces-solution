2f98780acd7e4268157a41d1100abdb4e8701011
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-04-11 15:35:55 +0600

    Create 977D - Divide by three, multiply by two.cpp

---
+++ 977D - Divide by three, multiply by two.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define out(a, b)               cout << a << " " << b << endl;
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = 1e9;
+const int N = 2e5+5;
+
+/*************User defined function*************/
+int cnt(ll x) {
+    int ans = 0;
+    while(x%3 == 0) ans++, x /= 3;
+    return ans;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<pair<int, ll>> v;
+    rep(i, 0, n) {
+        ll x;
+        cin >> x;
+        v.push_back({-cnt(x), x});
+    }
+
+    sort(all(v));
+
+    for(auto p: v) cout << p.second << ' ';   
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
