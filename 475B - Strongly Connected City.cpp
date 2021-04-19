62b21b0be44cafb72ef093436fddd62577faae41
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-04-19 23:54:40 +0600

    Create 475B - Strongly Connected City.cpp

---
+++ 475B - Strongly Connected City.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define out(a, b)               cout << a << " " << b << endl;
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = INT_MAX;
+const int N = 2e5+10;
+const int p = 1e9+7;
+
+/*************User defined function*************/
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    string a, b;
+    cin >> a >> b;
+    
+    if(a[0] == '>' && a[n-1] == '<'
+    && b[0] == '^' && b[m-1] == 'v') print("YES");
+    else if(a[0] == '<' && a[n-1] == '>'
+    && b[0] == 'v' && b[m-1] == '^') print("YES");
+    else print("NO");
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(9);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
