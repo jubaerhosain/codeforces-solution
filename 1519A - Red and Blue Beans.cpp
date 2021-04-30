ae8e2d0025b54a3d1ded73416fd187873f3b5bae
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-04-30 13:47:06 +0600

    Create 1519A - Red and Blue Beans.cpp

---
+++ 1519A - Red and Blue Beans.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define out(a, b)               cout << a << " " << b << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pii;
+typedef vector<vector<int>> vii;
+const ll inf = INT_MAX;
+const int N = 5e2+5;
+const int p = 1e9+7;
+
+/*************User defined function*************/
+void solve() {
+    ll a, b, d;
+    cin >> a >> b >> d;
+
+    if(a > b) swap(a, b);
+
+    if(b <= a*(d+1)) print("YES");
+    else print("NO");
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(12);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
