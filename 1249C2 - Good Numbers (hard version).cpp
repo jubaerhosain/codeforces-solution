4bec4258a61e43ce43dccaaa7fec8b481149639e
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-05-12 15:05:50 +0600

    Create 1249C2 - Good Numbers (hard version).cpp

---
+++ 1249C2 - Good Numbers (hard version).cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define out(a, b)               cout << a << " " << b << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = INT_MAX;
+const int p = 1e9+7;
+const int N = 1e6+5;
+
+/*************User defined function*************/
+vector<ll> a;
+vector<ll> x, y;
+ 
+void subset_sum(int n, int s) {
+    rep(i, 0, (1<<n)) {
+        ll curr = 0;
+        rep(j, 0, n) {
+            if(i & (1<<j)) curr += a[s+j];
+        }
+        if(s == 0) x.push_back(curr);
+        else y.push_back(curr);
+    }
+}
+
+void solve() {
+    int n = 40;
+
+    a = vector<ll>(n);
+    a[0] = 1LL;
+    rep(i, 1, n) {
+        a[i] = 3 * a[i-1];
+    }
+
+    subset_sum(n/2, 0);
+    subset_sum(n-n/2, n/2);
+    
+    int q;
+    cin >> q;
+
+    sort(all(x));
+    sort(all(y));
+    
+    while(q--) {
+        ll sum;
+        cin >> sum;
+
+        ll ans = a.back();
+        auto it1 = lower_bound(all(x), sum);
+        if(it1 != x.end()) ans = min(ans, *it1);
+        auto it2 = lower_bound(all(y), sum);
+        if(it2 != y.end()) ans = min(ans, *it2);
+
+        it2--;
+        ll tmp = sum - (*it2);
+        it1 = lower_bound(all(x), tmp);
+        if(it1 != x.end()) ans = min(ans, *it2 + *it1);
+
+        print(ans);
+    }
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(12);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
