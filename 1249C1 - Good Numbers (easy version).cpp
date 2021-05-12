a7e4bdebc04a2c30c93bd45d46b6b7d6bafcddb4
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-05-12 21:16:24 +0600

    Create 1249C1 - Good Numbers (easy version).cpp

---
+++ 1249C1 - Good Numbers (easy version).cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define pqueue                  priority_queue
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define out(a, b)               cout << a << " " << b << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+const ll inf = INT_MAX;
+const int N = 1e6+5;
+
+/*************User defined function*************/
+vector<ll> a;
+vector<ll> x;
+ 
+void subset_sum(int n, int s) {
+    rep(i, 0, (1<<n)) {
+        ll curr = 0;
+
+        rep(j, 0, n) 
+            if(i & (1<<j)) 
+                curr += a[s+j];
+
+        x.push_back(curr);
+    }
+}
+
+void solve() {
+    int n = 10;
+
+    a.resize(n);
+    a[0] = 1LL;
+    rep(i, 1, n) 
+        a[i] = 3 * a[i-1];
+    
+    subset_sum(n, 0);
+    sort(all(x));
+
+    int q;
+    cin >> q;
+    while(q--) {
+        ll sum;
+        cin >> sum;
+
+        auto it = lower_bound(all(x), sum);
+        print(*it);
+    }
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(12);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
