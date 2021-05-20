c28c6c832a3ca17783b07a0a9855c55849d8a183
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-05-20 21:50:18 +0600

    Create 916C - Jamie and Interesting Graph.cpp

---
+++ 916C - Jamie and Interesting Graph.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define out(a, b, c)            cout << a << " " << b << " " << c << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+/*************User defined function*************/
+const int inf = 1e9;
+const int N = 1e5+5;
+const int p = 1e6+3;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << p << " " << p << endl;
+    out(1, 2, p-(n-2));
+    rep(i, 2, n) out(i, i+1, 1);
+    
+    int cnt = n-1;
+    rep(i, 1, n+1) {
+        if(cnt == m) break;
+        rep(j, i+2, n+1) {
+            out(i, j, inf);
+            cnt++;
+            if(cnt == m) break;
+        }
+    }
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(12);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
