8ea9ea733321cebe72fda34d4defc0bab7d2b644
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-05-31 18:09:43 +0600

    Create 558C - Amr and Chemistry.cpp

---
+++ 558C - Amr and Chemistry.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define out(a, b, c)            cout << a << " " << b << " " << c << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+/*************User defined function*************/
+const ll inf = 1e9;
+const int N = 1e5+5;
+int cnt[N], steps[N], used[N];
+
+void solve() {
+    int n;
+    cin >> n;
+
+    rep(i, 1, n+1) {
+        int a;
+        cin >> a;
+
+        queue<pii> q;
+        q.push({a, 0});
+        while(!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            if(x > N || used[x] == i) continue;
+            cnt[x]++;
+            used[x] = i;
+            steps[x] += y;
+            q.push({x * 2, y + 1});
+            q.push({x / 2, y + 1});
+        }
+    }
+
+    int ans = inf;
+    rep(i, 0, N) {
+        if(cnt[i] == n)
+            ans = min(ans, steps[i]);
+    }
+
+    print(ans);
+} 
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(12);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
