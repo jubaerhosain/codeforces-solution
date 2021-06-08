f5804c784b32d65dc0eb7ff6bc0bd3505070b53d
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-06-08 22:55:58 +0600

    Create 1336A. Linova and Kingdom.cpp

---
+++ 1336A. Linova and Kingdom.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define endl                    '\n'//in interactive problem should not use this
+#define print(var)              cout << var << endl
+#define out(a, b)               cout << a << " " << b << endl
+#define rep(i, s, n)            for(int i = s; i < n; i++)
+#define rrep(i, n, s)           for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+/*************User defined function*************/
+const int N =2e5+5;
+vector<int> g[N];
+vector<int> dist;
+vector<int> mxcon;
+
+int dfs(int u, int p, int d) {
+    int cnt = 0;
+    for(int v: g[u]) {
+        if(v == p) continue;
+        cnt += dfs(v, u, d+1);
+    }
+    mxcon[u] = d - cnt;
+    return 1 + cnt;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    dist.resize(n+1);
+    mxcon.resize(n+1);
+    rep(i, 0, n-1) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    mxcon[0] = INT_MIN;
+    dfs(1, 0, 0);
+
+    sort(all(mxcon), greater<>());
+
+    ll ans = 0;
+    rep(i, 0, k) ans += mxcon[i];
+
+    print(ans);
+} 
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    cout << fixed << showpoint;
+    cout << setprecision(16);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}    
